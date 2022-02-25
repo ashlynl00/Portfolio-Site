dfe9a2d67fe13204a0b4c0dc6deeac1ca35380c3
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-02-25 15:31:17 -0800

    added more styling to home section

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ $('#homepage-content').on('mouseover', ()=>{
 
 let hamburgerCount = 0;
 
-$('#fixed-header').on('click', ()=>{
-    console.log('hello?');
-    hamburgerCount++;
-    if (hamburgerCount % 2 == 0) {
-        $('.nav-bar').css('opacity', '0.9');
-        $('#fixed-header img, header').css('z-index', '5');
-    } else {
-        $('.nav-bar').css('opacity', '0');
-        $('#fixed-header img, header').css('z-index', '5');
-    }
-    
- })
+if ($(window).width() < 480) {
+    $('#fixed-header').on('click', ()=>{
+        console.log('hello?');
+        console.log(hamburgerCount);
+        hamburgerCount++;
+        if (hamburgerCount % 2 !== 0) {
+            $('#homepage-content h1').css('opacity', '0.3');
+            $('.nav-bar').css('opacity', '0.9');
+            $('#fixed-header img, header').css('z-index', '5');
+        } else {
+            $('#homepage-content h1').css('opacity', '1');
+            $('.nav-bar').css('opacity', '0');
+            $('#fixed-header img, header').css('z-index', '5');
+        }
+     })
+}
+
+console.log()
 
