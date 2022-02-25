91e70492d10cb03feb3afee5c3df2045e9a602b5
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-02-25 10:09:38 -0800

    fixed hamburger menu

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ $('#homepage-content').on('mouseover', ()=>{
     $('.fancy-border').css('opacity', '0');
 })
 
-$('#fixed-header img').on('click', ()=>{
-    $('.nav-bar').css('opacity', '1')
-})
+let hamburgerCount = 0;
+
+$('#fixed-header').on('click', ()=>{
+    console.log('hello?');
+    hamburgerCount++;
+    if (hamburgerCount % 2 == 0) {
+        $('.nav-bar').css('opacity', '0.9');
+        $('#fixed-header img, header').css('z-index', '5');
+    } else {
+        $('.nav-bar').css('opacity', '0');
+        $('#fixed-header img, header').css('z-index', '5');
+    }
+    
+ })
 
