519c38bce1ec0fe8614734374966e683b1ee4180
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-02-28 13:59:17 -0800

    changed basic styling of page

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-$('#homepage-content').on('mouseover', ()=>{
-    $('.fancy-border').css('opacity', '1');
-    $('homepage-content').css('transform', 'scale(1.5)');
+$('.skill-container').on('mouseover', ()=>{
+    $('.skill-container').css('transform', 'scale(1.01)');
 }).mouseleave(()=>{
-    $('.fancy-border').css('opacity', '0');
+    $('.skill-container').css('transform', 'scale(1)');
 })
 
 let hamburgerCount = 0;
 
-if ($(window).width() < 480) {
-    $('#fixed-header').on('click', ()=>{
-        console.log('hello?');
-        console.log(hamburgerCount);
-        hamburgerCount++;
-        if (hamburgerCount % 2 !== 0) {
-            $('#homepage-content h1').css('opacity', '0.3');
-            $('.nav-bar').css('opacity', '0.9');
-            $('#fixed-header img, header').css('z-index', '5');
-        } else {
-            $('#homepage-content h1').css('opacity', '1');
-            $('.nav-bar').css('opacity', '0');
-            $('#fixed-header img, header').css('z-index', '5');
-        }
-     })
-}
+// if ($(window).width() < 480) {
+//     $('#fixed-header').on('click', ()=>{
+//         console.log('hello?');
+//         console.log(hamburgerCount);
+//         hamburgerCount++;
+//         if (hamburgerCount % 2 !== 0) {
+//             $('#homepage-content h1').css('opacity', '0.3');
+//             $('.nav-bar').css('opacity', '0.9');
+//             $('#fixed-header #hamburger, header').css('z-index', '5');
+//         } else {
+//             $('#homepage-content h1').css('opacity', '1');
+//             $('.nav-bar').css('opacity', '0');
+//             $('#fixed-header #hamburger, header').css('z-index', '5');
+//         }
+//      })
+// }
 
 console.log()
 
